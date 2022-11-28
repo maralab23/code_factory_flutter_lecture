ffb3b5a6f200730f4e68c80aaddd20143f57d019
maro <topdevlee@gmail.com>
2022-11-28 15:49:18 +0900

    lesson 50 - add settings_screen page

--- random_number_generator/lib/screen/settings_screen.dart
+++ random_number_generator/lib/screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/Color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -8,11 +9,50 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('SettingsScreen'),
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: maxNumber.toInt()
+                      .toString()
+                      .split('')
+                      .map(
+                        (e) => Image.asset(
+                          'asset/img/$e.png',
+                          height: 70.0,
+                          width: 50.0,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              Slider(
+                  value: maxNumber,
+                  min: 10000,
+                  max: 1000000,
+                  onChanged: (double value) {
+                    setState((){
+                      maxNumber = value;
+                    });
+                  }),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+                child: Text('저장!'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
