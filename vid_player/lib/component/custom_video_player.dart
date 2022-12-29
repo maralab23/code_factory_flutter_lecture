898a8be2a3405b70ecd8883da57f6d42d0d1874f
maro <topdevlee@gmail.com>
2022-12-29 23:17:06 +0900

    lesson 63 - add video player controller button

---
+++ vid_player/lib/component/custom_video_player.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+
+  const CustomVideoPlayer({
+    required this.video,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeController();
+  }
+
+  void initializeController() async {
+    videoPlayerController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoPlayerController!.initialize();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (videoPlayerController == null) {
+      return CircularProgressIndicator();
+    }
+
+    return AspectRatio(
+      aspectRatio: videoPlayerController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoPlayerController!,
+          ),
+          _Controls(
+            onReversePressed: onReversePressed,
+            onPlayPressed: onPlayPressed,
+            onForwardPressed: onForwardPressed,
+            isPlaying: videoPlayerController!.value.isPlaying,
+          ),
+          Positioned(
+            right: 0.0,
+            child: IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              iconSize: 30.0,
+              icon: Icon(
+                Icons.photo_camera_back,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onReversePressed() {
+
+  }
+
+  void onPlayPressed() {
+
+  }
+
+  void onForwardPressed() {
+    // 이미 실행중이면 중지
+    // 실행중이 아니면 실행
+    if (videoPlayerController!.value.isPlaying) {
+      videoPlayerController!.pause();
+    } else {
+      videoPlayerController!.play();
+    }
+  }
+
+}
+
+class _Controls extends StatelessWidget {
+  final VoidCallback onReversePressed;
+  final VoidCallback onPlayPressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controls({
+    required this.onReversePressed,
+    required this.onPlayPressed,
+    required this.onForwardPressed,
+    required this.isPlaying,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            iconData: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            iconData: isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            iconData: Icons.rotate_right,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(
+        iconData,
+      ),
+    );
+  }
+}
